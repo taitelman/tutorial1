2ea3c259c66919140c2be85360f4518a3851b049
taimor <taimor@il.ibm.com>
2018-02-16 18:52:27 +0200

    making sample work offline (download all js locally)

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+$(document).ready(function() {
+          $("#slider").slider({
+              animate: true,
+              value:1,
+              min: 0,
+              max: 1000,
+              step: 10,
+              slide: function(event, ui) {
+                  update(1,ui.value); //changed
+              }
+          });
+
+          update();
+});
+function update(slider,val) {
+ $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+}
+*/
